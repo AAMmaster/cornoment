9a7e67e7b5f83bb172b23e006481f15d90449346
Ali Asghar <120172954+AAMmaster@users.noreply.github.com>
2024-07-28 21:08:39 +0330

    update

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-let hour = document.getElementById("hour")
-let min = document.getElementById("min")
-let sec = document.getElementById("sec")
+let h1hour = document.getElementById("h1hour")
+let h1min = document.getElementById("h1min")
+let h1sec = document.getElementById("h1sec")
+
+
+
+const date = new Date();
+h1hour.innerText = date.getHours();
+h1min.innerText = date.getMinutes();
+h1sec.innerText = date.getSeconds();
+
+
+setInterval(() => {
+
+
+    const date = new Date();
+    h1hour.innerText = date.getHours();
+    h1min.innerText = date.getMinutes();
+    h1sec.innerText = date.getSeconds();
+
+} ,1000)
 
 
