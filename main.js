2cf2ae5ff868a35626b35622d690639f02ee3344
Ali Asghar <120172954+AAMmaster@users.noreply.github.com>
2024-07-31 23:15:22 +0330

    ali

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 let h1hour = document.getElementById("h1hour")
 let h1min = document.getElementById("h1min")
 let h1sec = document.getElementById("h1sec")
+let datediv = document.getElementById("date")
 
-
-
-const date = new Date();
-h1hour.innerText = date.getHours();
-h1min.innerText = date.getMinutes();
-h1sec.innerText = date.getSeconds();
 
 
 setInterval(() => {
@@ -15,8 +10,9 @@ setInterval(() => {
 
     const date = new Date();
     h1hour.innerText = date.getHours();
-    h1min.innerText = date.getMinutes();
+    h1min.innerHTML = `<span class="leftspan">:</span>25<span class="rightspan">:</span>`;
     h1sec.innerText = date.getSeconds();
+    
 
 }, 500)
 
@@ -30,6 +26,8 @@ cornoment.addEventListener("click", () => {
         cornoment.style.zIndex = ("2")
         cornoment.style.left = ("calc(50% + 8.5%)")
     }, 100);
+    
+
 })
 
 
