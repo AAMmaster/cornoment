50a17180b1673959c56b80ac8b6f07372ae72261
Ali Asghar <120172954+AAMmaster@users.noreply.github.com>
2024-08-01 04:37:35 +0330

    ali

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ let h1min = document.getElementById("h1min")
 let h1sec = document.getElementById("h1sec")
 let cornoment = document.getElementById("button")
 
+let houp = document.getElementById("houp")
+let hodawn = document.getElementById("hodawn")
+let minup = document.getElementById("minup")
+let mindawn = document.getElementById("mindawn")
+let secup = document.getElementById("secup")
+let secdawn = document.getElementById("secdawn")
+
+
 
 
 
@@ -18,9 +26,81 @@ let intelval = setInterval(() => {
 }, 500)
 
 
+function alertresulat() {
+
+
+
+    minnumber = 1
+
+    houp.addEventListener("click", () => {
+        h1hour.innerText++
+        h1hour = document.getElementById("h1hour")
+        console.log((h1hour.innerText));
+    })
+    hodawn.addEventListener("click", () => {
+        h1hour.innerText--
+        h1hour = document.getElementById("h1hour")
+        console.log(h1hour.innerText);
+    })
+    minup.addEventListener("click", () => {
+        h1min.innerHTML = `<span class="leftspan">:</span>${minnumber++}<span class="rightspan">:</span>`
+        h1min = document.getElementById("h1min")
+        console.log(minnumber);
+    })
+    mindawn.addEventListener("click", () => {
+        h1min.innerHTML = `<span class="leftspan">:</span>${minnumber--}<span class="rightspan">:</span>`
+        h1min = document.getElementById("h1min")
+        console.log(minnumber);
+    })
+    secup.addEventListener("click", () => {
+        h1sec.innerText++
+        h1sec = document.getElementById("h1sec")
+        console.log(h1sec.innerText);
+    })
+    secdawn.addEventListener("click", () => {
+        h1sec.innerText--
+        h1sec = document.getElementById("h1sec")
+        console.log(h1sec.innerText);
+    })
+
+    return `${h1hour.innerText}${minnumber}${h1sec.innerText}`
+}
+
+let alarm = setInterval(alertresulat(), 500);
+
 
 
 cornoment.addEventListener("click", () => {
+
+    
+
+
+    
+
+
+
+
+    houp.style.opacity = ("1")
+    hodawn.style.opacity = ("1")
+    minup.style.opacity = ("1")
+    mindawn.style.opacity = ("1")
+    secup.style.opacity = ("1")
+    secdawn.style.opacity = ("1")
+
+    houp.style.top = ("calc(50% - 85px)")
+    hodawn.style.top = ("calc(50% + 25px)")
+    minup.style.top = ("calc(50% - 85px)")
+    mindawn.style.top = ("calc(50% + 25px)")
+    secup.style.top = ("calc(50% - 85px)")
+    secdawn.style.top = ("calc(50% + 25px)")
+
+    houp.style.zIndex = ("3")
+    hodawn.style.zIndex = ("3")
+    minup.style.zIndex = ("3")
+    mindawn.style.zIndex = ("3")
+    secup.style.zIndex = ("3")
+    secdawn.style.zIndex = ("3")
+
 
     cornoment.style.zIndex = ("-1")
     cornoment.style.left = ("calc(50% + 8%)")
@@ -36,8 +116,8 @@ cornoment.addEventListener("click", () => {
     setTimeout(() => {
         cornoment.style.zIndex = ("2")
         cornoment.style.left = ("calc(50% + 8.5%)")
-        
-        
+
+
     }, 40);
 
 })
@@ -49,7 +129,29 @@ cornoment.addEventListener("click", () => {
 cornoment.addEventListener("dblclick", () => {
 
     
-    
+
+    houp.style.opacity = ("0")
+    hodawn.style.opacity = ("0")
+    minup.style.opacity = ("0")
+    mindawn.style.opacity = ("0")
+    secup.style.opacity = ("0")
+    secdawn.style.opacity = ("0")
+
+    houp.style.top = ("calc(50% - 5px)")
+    hodawn.style.top = ("calc(50% - 50px)")
+    minup.style.top = ("calc(50% - 5px)")
+    mindawn.style.top = ("calc(50% - 50px)")
+    secup.style.top = ("calc(50% - 5px)")
+    secdawn.style.top = ("calc(50% - 50px)")
+
+    houp.style.zIndex = ("-2")
+    hodawn.style.zIndex = ("-2")
+    minup.style.zIndex = ("-2")
+    mindawn.style.zIndex = ("-2")
+    secup.style.zIndex = ("-2")
+    secdawn.style.zIndex = ("-2")
+
+
     intelval = setInterval(() => {
 
         const date = new Date();
@@ -58,6 +160,9 @@ cornoment.addEventListener("dblclick", () => {
         h1sec.innerText = date.getSeconds();
 
     }, 500);
+
+
+    clearInterval(alarm)
 
 },)
 
